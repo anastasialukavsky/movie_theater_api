20409cd2a1a82b6822f8f78220dc15e6f9646555
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-12 23:21:48 -0400

    rename movieRouter to show router

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRoutes');
-const movieRouter = require('./routes/showRoutes')
+const showRouter = require('./routes/showRoutes')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/users', userRouter);
-app.use('/movies', movieRouter);
+app.use('/shows', showRouter);
 
 
 
